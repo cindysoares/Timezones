b0bb80a3e670f09df6b46b6914388b695982fff7
Cindy De Araujo Soares Moore <cindy.as@gmail.com>
2016-03-08 18:57:55 -0300

    adding, updating and deleting a time zone

--- src/main/webapp/js/entries.js
+++ src/main/webapp/js/entries.js
@@ -1,7 +1,48 @@
 (function(){
 	var timezonesApp = angular.module('entries', ['ngMessages']);
 		
-	timezonesApp.controller('EntriesCtrl', function($scope, $filter) {
+	timezonesApp.factory('addTimezoneService',  function($http) {
+		var myService = {
+				async: function (userId, timezone) {
+					var promise = $http.post("/timezones/add/" + userId, null, 
+							{params: {name: timezone.name, city: timezone.city, gmtDifference: timezone.gmtDifference}})
+					.then(function(response){
+						return response.data;
+					});
+					return promise;
+				}
+		};
+		return myService;
+	});
+	
+	timezonesApp.factory('removeTimezoneService',  function($http) {
+		var myService = {
+				async: function (userId, timezoneId) {
+					var promise = $http.delete("/timezones/remove/" + userId + "/" + timezoneId)
+					.then(function(response){
+						return response.data;
+					});
+					return promise;
+				}
+		};
+		return myService;
+	});
+	
+	timezonesApp.factory('updateTimezoneService',  function($http) {
+		var myService = {
+				async: function (userId, timezone) {
+					var promise = $http.post("/timezones/update/" + userId + "/" + timezone.id, null, 
+							{params: {name: timezone.name, city: timezone.city, gmtDifference: timezone.gmtDifference}})
+					.then(function(response){
+						return response.data;
+					});
+					return promise;
+				}
+		};
+		return myService;
+	});	
+	
+	timezonesApp.controller('EntriesCtrl', function($scope, $filter, addTimezoneService, removeTimezoneService, updateTimezoneService) {
 		this.$messages = {}
 		this.editedTimezone = {};
 		this.editMode = false;
@@ -36,7 +77,52 @@
 			this.$messages = {};
 			this.filters = {};
 		};
-		
+		this.removeTimezone = function(timezoneToRemove) {
+			this.selectedIndex = -1
+			var timezonesArray = eval( this.selectedUser.timezones );
+			for( var i = 0; i < timezonesArray.length; i++ ) {
+				if( timezonesArray[i].id === timezoneToRemove.id ) {
+					this.selectedIndex = i;
+					break;
+				}
+			}
+			if( this.selectedIndex === -1 ) {
+				this.$messages.warning = true;
+			}
+			removeTimezoneService.async(this.selectedUser.id, timezoneToRemove.id).then(function(removed) {
+				if (removed) {
+					$scope.editTimezone.selectedUser.timezones.splice( $scope.editTimezone.selectedIndex, 1 );
+					$scope.editTimezone.$messages.deleteSuccess = true;
+				} else {
+					$scope.editTimezone.$messages.warning = true;
+				}
+			});			
+		};
+		this.addTimezone = function() {
+			addTimezoneService.async(this.selectedUser.id, this.editedTimezone).then(function(d){
+				if (d != null) {
+					$scope.editTimezone.selectedUser.timezones.push(d);
+					$scope.editTimezone.editedTimezone = {};
+					$scope.editTimezone.editMode = false;
+					$scope.editTimezone.$messages.saveSuccess = true;
+				} else {
+					$scope.editTimezone.$messages.warning = true;
+				}
+			});			
+		};
+		this.updateTimezone = function(timezoneToUpdate) {
+			this.selectedIndex = this.selectedUser.timezones.indexOf(timezoneToUpdate);
+			updateTimezoneService.async(this.selectedUser.id, timezoneToUpdate).then(function(d){				
+				if (d != null) {
+					$scope.editTimezone.selectedUser.timezones.splice( $scope.editTimezone.selectedIndex, 1 );
+					$scope.editTimezone.selectedUser.timezones.push(d);
+					$scope.editTimezone.editMode = false;
+					$scope.editTimezone.$messages.updateSuccess = true;
+				} else {
+					$scope.editTimezone.$messages.warning = true;
+				}
+			});		
+		};		
 		$scope.$on("tabSelected", function(event, args){
 			if (args.selectedTab === 'timezones') {				
 				if($scope.section.selectedUser) {
