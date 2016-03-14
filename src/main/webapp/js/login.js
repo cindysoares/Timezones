f36399ca81322f63c87b807cfe8ca822df5ca948
Cindy De Araujo Soares Moore <cindy.as@gmail.com>
2016-03-14 15:58:45 -0300

    login using a token

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -4,21 +4,25 @@
 	
 	loginApp.factory('loginFactory', function($http) {
 		var myService = {
-				async: function (emailValue, passwordValue) {
-					var promise = $http.post('/login', {email: emailValue, password: passwordValue}, 
-							{headers: {'Content-Type': "application/x-www-form-urlencoded"},
-							 transformRequest: function(obj) {
-					                var str = [];
-					                for(var p in obj)
-					                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-					                return str.join("&");
+				login: function (emailValue, passwordValue) {
+					var promise = $http.post('/login', {email: emailValue, password: passwordValue},
+							{transformResponse: function(obj) {
+					                return obj;
 					            }
 							})
 							.then(function(response){
-								return response.data;
+								var token = response.data;
+								$http.defaults.headers.common.Authorization = token;
+								return token;
 							});
 					return promise;
-				} // FIXME Don´t pass password as a param.
+				},
+				getLoggedUser: function() {
+					return $http.get('/login')
+						.then(function(response){
+							return response.data;
+						});
+				}
 		};
 		return myService;
 	});
@@ -27,14 +31,23 @@
 		this.email = "cindy@email.com"; // FIXME remove this value 
 		this.password = "senha"; // FIXME remove this value 
 		this.$messages = {};
-		this.submit = function() {
-			loginFactory.async(this.email, this.password).then(function(d) {				
+		this.submit = function($http) {
+			loginFactory.login(this.email, this.password).then(function(d) {				
 				if(!d) {
 					$scope.login.$messages.invalidLogin = true;
 				} else {
-					$scope.timezones.login(d);
-					$scope.login.$messages = {}
+					$scope.login.$messages = {};
+					loginFactory.getLoggedUser().then(function(d) {				
+						if(!d) {
+							$scope.login.$messages.invalidLogin = true;
+						} else {
+							$scope.timezones.login(d);
+							$scope.login.$messages = {}
+						}
+					});					
 				}
+			}, function(error) {
+				$scope.login.$messages.invalidLogin = true;
 			});
 		};
 	});  
